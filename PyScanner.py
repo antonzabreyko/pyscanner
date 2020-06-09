264c033815ac85e2bfcc31d4bb143d16d8c50b9c
antonzabreyko <anton.zabreyko@gmail.com>
2020-06-08 22:30:27 -0700

    Shortened code further

--- PyScanner.py
+++ PyScanner.py
@@ -8,20 +8,15 @@ class PyScanner():
     def next(self):
         if (not self.hasNext()):
             raise IndexError("index out of bounds")
-        k = None
-        for i in range(len(self.string)):
-            if self.string[i] != self.separator:
-                k = i
-                break
 
         g = len(self.string)
 
-        for i in range(k, len(self.string)):
+        for i in range(len(self.string)):
             if (self.string[i] == self.separator):
                 g = i
                 break
 
-        partition = self.string[k:g]
+        partition = self.string[:g]
         self.string = self.string[g:]
         return partition
 
