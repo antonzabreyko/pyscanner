c4625d58a3e86a93afa6212abaae69731c0bbc98
antonzabreyko <anton.zabreyko@gmail.com>
2020-06-11 19:26:11 -0700

    Added generic __next__ method

--- PyScanner.py
+++ PyScanner.py
@@ -1,39 +1,41 @@
 import re
+''' Scanner object for strings. Based on Java's scanners. '''
 class PyScanner():
 
+    ''' Initialization method. Takes in the string to be scanned, and an
+        optional separator character. By default the separator is whitespace. '''
     def __init__(self, string, separator=" "):
         self.string = string
-        self.separator = separator
+        self.separator = "[^" + separator + "]+"
 
 
-    def next(self):
-        cond, index = self.hasNext(True)
+    def __next__(self, param):
+        cond, start, end = self.__hasNext__(param)
         if (not cond):
             raise IndexError("index out of bounds")
 
-        k = index
-
-        g = len(self.string)
-
-        for i in range(k, len(self.string)):
-            if (self.string[i] == self.separator):
-                g = i
-                break
-
-        partition = self.string[k:g]
-        self.string = self.string[g:]
+        partition = self.string[start:end]
+        self.string = self.string[end:]
         return partition
 
+
+    ''' Returns the next partition of the string, closed off by the separator. '''
+    def next(self):
+        return self.__next__(self.separator)
+
     def hasNext(self, index=False):
-        results = PyScanner.__hasNext__(self, "[^" + self.separator + "]")
+        results = PyScanner.__hasNext__(self, self.separator)
         return index and results or results[0]
 
     def __hasNext__(self, param):
         match = re.search(param, self.string)
         if not match:
-            return (False, -1)
-        return (True, match.start())
+            return (False, -1, -1)
+        return (True, match.start(), match.end())
 
     def hasNextInt(self):
-        regex = "?[-][0-9]+"
-        return self.__hasNext__(regex)
+        regex = "[-]?[0-9]+"
+        return self.__hasNext__(regex)[0]
+
+    def nextInt(self):
+        regex = regex = "[-]?[0-9]+"
